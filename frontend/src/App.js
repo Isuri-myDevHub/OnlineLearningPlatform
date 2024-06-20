a466a0dc9b52b5a8789671941d1a9bb28c4d0517
isuri <isuriwgunaratne@gmail.com>
2024-06-20 10:53:07 +0530

    Setup Routing in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
+import CourseList from './components/Courses/CourseList';
+import CourseDetail from './components/Courses/CourseDetail';
+import EnrollmentList from './components/Enrollments/EnrollmentList';
+import UserList from './components/Users/UserList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [token, setToken] = useState('');
+
+    return (
+        <Router>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/register">Register</Link>
+                <Link to="/login">Login</Link>
+                <Link to="/courses">Courses</Link>
+                <Link to="/enrollments">Enrollments</Link>
+                <Link to="/users">Users</Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login setToken={setToken} />} />
+                <Route path="/courses" element={<CourseList token={token} />} />
+                <Route path="/courses/:id" element={<CourseDetail token={token} />} />
+                <Route path="/enrollments" element={<EnrollmentList token={token} />} />
+                <Route path="/users" element={<UserList token={token} />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
