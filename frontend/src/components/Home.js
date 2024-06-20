84fc38739fa9fc302e5e33406a80e631e141905f
isuri <isuriwgunaratne@gmail.com>
2024-06-20 11:50:09 +0530

    Add Home Component and Bootstrap

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+// src/components/Home.js
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Welcome to the Online Learning Platform</h2>
+        </div>
+    );
+};
+
+export default Home;
