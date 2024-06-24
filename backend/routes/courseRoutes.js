cd3c3f48c3c8a7764959c8df10c665496dfaafdc
Isuri Wijesinghe Gunaratne <75952849+IsuriGunaratne@users.noreply.github.com>
2024-06-24 11:59:41 +0530

    Update courseRoutes.js

--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -1,27 +1,11 @@
-// const express = require('express');
-// const router = express.Router();
-// const courseController = require('../controllers/courseController');
-
-// router.post('/', courseController.createCourse);
-// router.get('/', courseController.getAllCourses);
-// router.get('/:id', courseController.getCourseById);
-// router.put('/:id', courseController.updateCourse);
-// router.delete('/:id', courseController.deleteCourse);
-
-// module.exports = router;
-
-
-// routes/courseRoutes.js
-
 const express = require('express');
 const router = express.Router();
-const CourseController = require('../controllers/courseController');
+const courseController = require('../controllers/courseController');
 
-// Routes for CRUD operations on courses
-router.post('/courses', CourseController.createCourse);
-router.get('/courses', CourseController.getAllCourses);
-router.get('/courses/:id', CourseController.getCourseById);
-router.put('/courses/:id', CourseController.updateCourse);
-router.delete('/courses/:id', CourseController.deleteCourse);
+router.post('/', courseController.createCourse);
+router.get('/', courseController.getAllCourses);
+router.get('/:id', courseController.getCourseById);
+router.put('/:id', courseController.updateCourse);
+router.delete('/:id', courseController.deleteCourse);
 
 module.exports = router;
