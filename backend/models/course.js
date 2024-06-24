08d96ec8a7f93bea7ed1efc30547e94650cea39a
Isuri Wijesinghe Gunaratne <75952849+IsuriGunaratne@users.noreply.github.com>
2024-06-24 11:59:12 +0530

    Update course.js

--- backend/models/course.js
+++ backend/models/course.js
@@ -1,26 +1,25 @@
-// ../models/course.js
-
-const { DataTypes, Model } = require('sequelize');
-const sequelize = require('../config/database');
-
-class Course extends Model {}
-
-Course.init({
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  description: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  instructor: {
-    type: DataTypes.STRING,
-    allowNull: false
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Course extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
   }
-}, {
-  sequelize,
-  modelName: 'Course',
-});
-
-module.exports = Course;
+  Course.init({
+    title: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    instructor: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Course',
+  });
+  return Course;
+};
